60f293fd692f56176a82b66ff1f662313b510073
Dawid Spiechowicz <spiechu@gmail.com>
2012-07-09 09:36:00 +0300

    add dir const and some docblock

--- tests/JSONDiskCacheTest/JSONDiskCacheTest.php
+++ tests/JSONDiskCacheTest/JSONDiskCacheTest.php
@@ -17,6 +17,11 @@ class JSONDiskCacheTest extends \PHPUnit_Framework_TestCase
 {
 
     const DOMAIN = 'testdomain';
+    
+    /**
+     * Relative path to cache dir
+     */
+    const TEST_CACHE_DIR = '/../../testcache';
 
     /**
      * @var JsonDiskCache
@@ -24,7 +29,7 @@ class JSONDiskCacheTest extends \PHPUnit_Framework_TestCase
     protected $_jsonDiskCache;
 
     /**
-     * Path to testcache dir
+     * Full path to cache dir
      *
      * @var string
      */
@@ -39,11 +44,16 @@ class JSONDiskCacheTest extends \PHPUnit_Framework_TestCase
      */
     protected static $_integerTestValues = [[-12345], [-5], [0], [5], [12345]];
 
+    /**
+     * String values to test
+     *
+     * @var array
+     */
     protected static $_stringTestValues =  [[''], ['hello'], ['0'], ['This is short text'], ['Stolen from www.lipsum.com: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat']];
 
     protected function setUp()
     {
-        $this->_cacheDirPath = __DIR__ . '/../../testcache';
+        $this->_cacheDirPath = __DIR__ . self::TEST_CACHE_DIR;
         $this->_jsonDiskCache = new JSONDiskCache($this->_cacheDirPath, self::DOMAIN);
     }
 
@@ -53,8 +63,7 @@ class JSONDiskCacheTest extends \PHPUnit_Framework_TestCase
         unset($this->_jsonDiskCache);
 
         foreach (scandir($this->_cacheDirPath) as $file) {
-            if ($file !== '.' &&
-            $file !== '..' &&
+            if ($file !== '.' && $file !== '..' &&
             // PHP 5.4 (new Object)->method() call in one line
             (new \SplFileInfo($this->_cacheDirPath . '/' . $file))->getExtension() === 'cache') {
                 unlink($this->_cacheDirPath . '/' . $file);
